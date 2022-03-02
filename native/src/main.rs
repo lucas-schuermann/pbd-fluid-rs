83a9543253044c4fb807b6e654c0cb87a6602df1
Lucas V. Schuermann <lucas@lvs.io>
2022-03-02 00:08:21 -0500

    change default point size

--- native/src/main.rs
+++ native/src/main.rs
@@ -8,7 +8,7 @@ const DAM_PARTICLES_X: usize = 10;
 const DAM_PARTICLES_Y: usize = 1000;
 const BLOCK_PARTICLES: usize = 500;
 const MAX_PARTICLES: usize = solver::MAX_PARTICLES;
-const POINT_SIZE: f32 = 5.0;
+const POINT_SIZE: f32 = 7.5;
 const BOUNDARY_COLOR: [f32; 4] = [112.0 / 255.0, 128.0 / 255.0, 144.0 / 255.0, 1.0]; // #708090
 const PARTICLE_COLOR: [f32; 4] = [65.0 / 255.0, 105.0 / 255.0, 1.0, 1.0]; // #4169E1
 
